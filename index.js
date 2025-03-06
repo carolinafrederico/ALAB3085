ba14c8fc37d02921c4bf33cfffe1a132f3152c85
carolinafrederico <iam@carolinafrederico.com>
2025-03-06 14:26:59 -0500

    third exercise

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ function addSum(array) {
 console.log(addSum(nums))
 
 // Take an array of numbers and return the average.
+
 function avaSum(array) {
     return addSum(array) / array.length
 }
@@ -18,3 +19,14 @@ console.log(avaSum(nums))
 
 // Take an array of strings and return the longest string.
 
+const myString=["JavaScript", "is", "harder", "than", "life"]
+function getLongestString(stringArray) {
+let longestString=""
+for (let index = 0; index < myString.length; index++) {
+    if (stringArray[index].length > longestString.length){
+        longestString=stringArray[index]
+    }    
+}
+return longestString
+}
+console.log(getLongestString(myString))
